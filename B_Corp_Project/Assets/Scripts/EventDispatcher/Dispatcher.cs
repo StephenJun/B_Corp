8fe4414114700fd2192fd656753f157dcbea2d3b
Stephen <stephenmailbox@163.com>
2020-10-29 23:14:12 +0800

    eventdispatcher

---
+++ B_Corp_Project/Assets/Scripts/EventDispatcher/Dispatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispatcher
+{
+    private Dictionary<string, List<Action>> listeners;
+
+	public Dispatcher()
+    {
+		listeners = new Dictionary<string, List<Action>>();
+	}
+
+	public void Add(string eventName, Action callback)
+    {
+		listeners[eventName].Add(callback);
+	}
+
+	public void Remove(string eventName, Action callback)
+    {
+        if (listeners.ContainsKey(eventName))
+        {
+			listeners[eventName].Remove(callback);
+		}
+	}
+
+	public bool HasListener(string eventName, Action listener)
+    {
+		if (listeners.ContainsKey(eventName))
+			return listeners[eventName].Contains(listener);
+		return false;
+	}
+
+	public void Dispatch(string eventName)
+    {
+        foreach (var listener in listeners[eventName])
+        {
+			listener();
+		}
+    }
+}
---
+++ B_Corp_Project/Assets/Scripts/EventDispatcher/EventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+namespace zFrame.Event
+{
+    /// <summary>
+    /// 事件信息类基类
+    /// </summary>
+    public abstract class BaseEventArgs:IDisposable
+    {
+        public  Enum EventType { protected set; get; }
+        public GameObject Sender { protected set; get; }
+        public virtual void Config(Enum _t, GameObject _sender)
+        {
+            EventType = _t;
+            Sender = _sender;
+        }
+
+        /// <summary>
+        ///  但事件信息类被回收时调用
+        /// </summary>
+        public  virtual void Dispose()
+        {
+            this.Sender = null;
+        }
+    }
+  
+    
+}
+
